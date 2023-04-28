70f538d3f412309ef09c04c5309a6c8cb43d1564
HudsonBean <hbean2025@icloud.com>
2023-04-28 14:06:55 -0500

    Added fade scroll effect

--- Index.js
+++ Index.js
@@ -65,3 +65,17 @@ for(let i = 0; i < a.length; i++) {
         setVideo(i+1);
     };
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry.target)
+        if (entry.isIntersecting) {
+            entry.target.classList.remove('show');
+        } else {
+            entry.target.classList.add('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
